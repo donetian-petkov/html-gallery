260aff890d266359c5211d5722493a2f93c4c124
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2023-09-19 15:55:27 +0300

    Filtering

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 import imageList from "./imageList.js";
 
 const gallery = document.getElementById('gallery');
+const buttons = document.querySelectorAll('[id*="button_"]');
 
-const populateGallery = (tag, index = 0) => {
+buttons.forEach(button =>
+    button.addEventListener('click', function() {
+        populateGallery(this.name);
+    }))
 
-    const images = imageList.find(item => item.tag === tag)?.images;
 
-   images?.forEach(x => {
-       const divElement = document.createElement('div');
-       const imgElement = document.createElement('img');
-       divElement.className = "gallery-item";
-       imgElement.src = x;
-       imgElement.alt = `nature${index++}`;
-       divElement.appendChild(imgElement)
-       gallery.appendChild(divElement);
-   });
+const populateGallery = (tag = "all", index = 0) => {
+
+    gallery.replaceChildren();
+
+    let images;
+
+    if (tag !== "all") {
+        images = imageList.find(item => item.tag === tag)?.images;
+    } else {
+        images = imageList.map(item => item.images).flat();
+    }
+
+    images?.forEach(x => {
+        const divElement = document.createElement('div');
+        const imgElement = document.createElement('img');
+        divElement.className = "gallery-item";
+        imgElement.src = x;
+        imgElement.alt = `nature${index++}`;
+        divElement.appendChild(imgElement)
+        gallery.appendChild(divElement);
+    });
 
 }
 
-populateGallery("nature");
+
+
+populateGallery();
